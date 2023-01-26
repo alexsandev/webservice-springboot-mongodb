82425530f64ecdd441242c1bd1ba1a1204ea941d
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-25 21:48:57 -0300

    Creating entity Post with nested User

--- src/main/java/com/alexsandev/config/Instantiation.java
+++ src/main/java/com/alexsandev/config/Instantiation.java
@@ -1,29 +1,44 @@
 package com.alexsandev.config;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.alexsandev.domain.Post;
 import com.alexsandev.domain.User;
+import com.alexsandev.repositories.PostRepository;
 import com.alexsandev.repositories.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
         
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
     } 
 }
