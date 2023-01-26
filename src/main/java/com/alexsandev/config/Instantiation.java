0e89d5bd16c9b35d02027dc2c668f9b9e6639f55
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-25 21:59:29 -0300

    Projection of author data with DTO

--- src/main/java/com/alexsandev/config/Instantiation.java
+++ src/main/java/com/alexsandev/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.alexsandev.domain.Post;
 import com.alexsandev.domain.User;
+import com.alexsandev.dto.AuthorDTO;
 import com.alexsandev.repositories.PostRepository;
 import com.alexsandev.repositories.UserRepository;
 
@@ -34,10 +35,10 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, LocalDateTime.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDateTime.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
     } 
