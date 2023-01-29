9e3f056572a2b1be52770ba70dd6e4a96ffcebe5
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-28 22:29:16 -0300

    Simple query with Spring query methods

--- src/main/java/com/alexsandev/resources/PostResource.java
+++ src/main/java/com/alexsandev/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.alexsandev.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexsandev.domain.Post;
+import com.alexsandev.resources.util.URL;
 import com.alexsandev.services.PostService;
 
 @RestController
@@ -20,5 +24,10 @@ public class PostResource {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findbyId(@PathVariable String id){
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        return ResponseEntity.ok().body(service.findByTitle(URL.decodeParam(text)));
     }
 }
